d2191fc4f510dd1b1db6248ef66346f2c2e6fd94
Ravil Bikbulatov <weeellz12@gmail.com>
2020-11-03 21:43:42 +0300

    Add bucket sort

--- src/c_api.cpp
+++ src/c_api.cpp
@@ -2,11 +2,11 @@
 #include "algo/sorting.hpp"
 
 void radix_argsort_u16(uint16_t *arr, uint16_t *indices, uint64_t size) {
-  algo::radix_argsort(arr, arr + size, indices, indices + size);
+  algo::radix_argsort(arr, arr + size, indices);
 }
 void bucket_argsort_u16(uint16_t *arr, uint16_t *indices, uint64_t size) {
-  algo::bucket_argsort(arr, arr + size, indices, indices + size);
+  algo::bucket_argsort(arr, arr + size, indices);
 }
 void quick_argsort_u16(uint16_t *arr, uint16_t *indices, uint64_t size) {
-  algo::bucket_argsort(arr, arr + size, indices, indices + size);
+  algo::bucket_argsort(arr, arr + size, indices);
 }
