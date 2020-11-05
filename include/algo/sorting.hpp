08978ec0e1102015cad4bd68706299bb511eca59
Ravil Bikbulatov <weeellz12@gmail.com>
2020-11-05 20:50:57 +0300

    Add radix sort

--- include/algo/sorting.hpp
+++ include/algo/sorting.hpp
@@ -3,6 +3,7 @@
 #include <concepts>
 #include <iterator>
 #include <math.h>
+#include <numeric>
 #include <vector>
 
 namespace algo {
@@ -34,6 +35,38 @@ void insertion_sort(Iter begin, Iter end, Comp comp) {
     }
   }
 }
+
+template <typename Iter, typename ResultIter>
+void radix_argsort_impl(Iter begin, Iter end, ResultIter result, int i) {
+  auto size = std::distance(begin, end);
+  auto bytes_count = sizeof(std::iter_value_t<Iter>);
+  auto bits_count = bytes_count * 8;
+  if (size < 2 || i >= bits_count) {
+    return;
+  }
+  auto iter_begin = begin;
+  auto iter_end = begin + size - 1;
+  for (; iter_begin != iter_end;) {
+    auto ith_greatest_bit_first = ((*iter_begin) >> ((bits_count - 1) - i)) & 1;
+    auto ith_greatest_bit_second = ((*iter_end) >> ((bits_count - 1) - i)) & 1;
+    if (ith_greatest_bit_first == 0) {
+      ++iter_begin;
+    } else if (ith_greatest_bit_second == 1) {
+      --iter_end;
+    } else {
+      std::swap(*iter_begin, *iter_end);
+      auto i = std::distance(begin, iter_begin);
+      auto j = std::distance(begin, iter_end);
+      std::swap(*(result + i), *(result + j));
+    }
+  }
+  auto ith_greatest_bit_first = ((*iter_begin) >> ((bits_count - 1) - i)) & 1;
+  if (ith_greatest_bit_first == 0) {
+    ++iter_begin;
+  }
+  radix_argsort_impl(begin, iter_begin, result, i + 1);
+  radix_argsort_impl(iter_begin, end, result, i + 1);
+}
 } // namespace detail
 
 template <typename Iter, typename ResultIter>
@@ -41,24 +74,9 @@ void radix_argsort(Iter begin, Iter end,
                    ResultIter result) requires std::random_access_iterator<Iter>
     &&std::unsigned_integral<std::iter_value_t<Iter>>
         &&detail::index_iter<ResultIter> {
-  auto iter_end = end;
-  auto i = 0;
-  for (auto iter_begin = begin; iter_begin != iter_end; ++iter_begin) {
-    if (iter_begin == iter_end) {
-      auto iter_mid = iter_begin;
-      radix_argsort(begin, iter_mid, result);
-      radix_argsort(iter_mid, end, result);
-    }
-    auto bytes_count = sizeof(std::iter_value_t<Iter>);
-    auto bits_count = bytes_count * 8;
-    auto ith_greatest_bit_first = (*iter_begin >> ((bits_count - 1) - i)) & 1;
-    auto ith_greatest_bit_second = (*iter_end >> ((bits_count - 1) - i)) & 1;
-    if (ith_greatest_bit_first > ith_greatest_bit_second) {
-      std::swap(*iter_begin, *iter_end);
-    }
-    --iter_end;
-    ++i;
-  }
+  std::vector tmp(begin, end);
+  std::iota(result, result + std::distance(begin, end), 0);
+  detail::radix_argsort_impl(tmp.begin(), tmp.end(), result, 0);
 }
 
 template <typename Iter, typename ResultIter>
--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,21 @@
 #include "algo/sorting.hpp"
 #include <iostream>
-#include <iterator>
-
-template <typename Iter, typename ResultIter>
-void radix_argsort(Iter begin, Iter end, ResultIter result, int i) {
-  auto bytes_count = sizeof(std::iter_value_t<Iter>);
-  auto bits_count = bytes_count * 8;
-  if (std::distance(begin, end) < 2 || i >= bits_count) {
-    return;
-  }
-  auto iter_begin = begin;
-  auto iter_end = begin + std::distance(begin, end) - 1;
-  for (; iter_begin != iter_end;) {
-    auto bytes_count = sizeof(std::iter_value_t<Iter>);
-    auto bits_count = bytes_count * 8;
-    auto first = *iter_begin;
-    auto second = *iter_end;
-    auto ith_greatest_bit_first = ((*iter_begin) >> ((bits_count - 1) - i)) & 1;
-    auto ith_greatest_bit_second = ((*iter_end) >> ((bits_count - 1) - i)) & 1;
-    if (ith_greatest_bit_first == 0) {
-      ++iter_begin;
-    } else if (ith_greatest_bit_second == 1) {
-      --iter_end;
-    } else {
-      std::swap(*iter_begin, *iter_end);
-    }
-  }
-  auto ith_greatest_bit_first = ((*iter_begin) >> ((bits_count - 1) - i)) & 1;
-  if (ith_greatest_bit_first == 0) {
-    ++iter_begin;
-  }
-  radix_argsort(begin, iter_begin, result, i + 1);
-  radix_argsort(iter_begin, end, result, i + 1);
-}
 
 int main() {
-  // std::vector<int> v = {1, 3, 2, 7, 5, 4, 3, 9};
-  int i = 0;
-  std::vector<int> v = {1, 4, 3, 2};
-  std::vector<int> result(v.size());
-  radix_argsort(v.begin(), v.end(), result.begin(), i);
+  std::vector<uint32_t> v = {1, 3, 2, 7, 5, 4, 3, 9};
+  // std::vector<uint32_t> v = {1, 4, 3, 2};
+  std::vector<uint32_t> result(v.size());
+  algo::radix_argsort(v.begin(), v.end(), result.begin());
   for (auto element : v) {
-    std::cout << element << std::endl;
+    std::cout << element << " ";
   }
-  // for (auto element : result) {
-  //   std::cout << v[element] << std::endl;
-  // }
+  std::cout << std::endl;
+  for (auto element : result) {
+    std::cout << element << " ";
+  }
+  std::cout << std::endl;
+  for (auto element : result) {
+    std::cout << v[element] << " ";
+  }
+  std::cout << std::endl;
 }
