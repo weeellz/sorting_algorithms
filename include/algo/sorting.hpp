93ceb186751bffe4357cdc0c36b8c74176a66855
Ravil Bikbulatov <weeellz12@gmail.com>
2020-11-25 14:06:20 +0300

    Move experimental radix sort

--- include/algo/sorting.hpp
+++ include/algo/sorting.hpp
@@ -77,39 +77,7 @@ void counting_argsort_impl(Iter begin, Iter end, ResultIter result,
 }
 
 template <typename Iter, typename ResultIter>
-void radix_argsort_impl(Iter begin, Iter end, ResultIter result, int i) {
-  auto size = std::distance(begin, end);
-  auto bits_count = sizeof(std::iter_value_t<Iter>) * 8;
-  if (size < 2 || i >= bits_count) {
-    return;
-  }
-  auto iter_begin = begin;
-  auto iter_end = begin + size - 1;
-  while (iter_begin != iter_end) {
-    auto ith_greatest_bit_begin = ((*iter_begin) >> ((bits_count - 1) - i)) & 1;
-    auto ith_greatest_bit_end = ((*iter_end) >> ((bits_count - 1) - i)) & 1;
-    if (ith_greatest_bit_begin == 0) {
-      ++iter_begin;
-    } else if (ith_greatest_bit_end == 1) {
-      --iter_end;
-    } else {
-      std::iter_swap(iter_begin, iter_end);
-      auto i = std::distance(begin, iter_begin);
-      auto j = std::distance(begin, iter_end);
-      std::iter_swap(result + i, result + j);
-    }
-  }
-  auto ith_greatest_bit_begin = ((*iter_begin) >> ((bits_count - 1) - i)) & 1;
-  if (ith_greatest_bit_begin == 0) {
-    ++iter_begin;
-  }
-  radix_argsort_impl(begin, iter_begin, result, i + 1);
-  radix_argsort_impl(iter_begin, end, result + std::distance(begin, iter_begin),
-                     i + 1);
-}
-
-template <typename Iter, typename ResultIter>
-void radix_counting_argsort_impl(Iter begin, Iter end, ResultIter result) {
+void radix_argsort_impl(Iter begin, Iter end, ResultIter result) {
   if (std::distance(begin, end) < 2) {
     return;
   }
@@ -248,7 +216,7 @@ void radix_argsort(Iter begin, Iter end,
         &&detail::index_iter<ResultIter> {
   std::vector tmp(begin, end);
   std::iota(result, result + std::distance(begin, end), 0);
-  detail::radix_counting_argsort_impl(tmp.begin(), tmp.end(), result);
+  detail::radix_argsort_impl(tmp.begin(), tmp.end(), result);
 }
 
 template <typename Iter, typename ResultIter>
